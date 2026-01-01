917a0b0330b605d571599dac279630ce256267b3
Vamsi Krishna <iam.vamsitupakula@gmail.com>
2026-01-01 11:58:37 +0530

    chore: add more plugins

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,36 @@
+return {
+	{
+		"mason-org/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "ts_ls", "jdtls", "yamlls" },
+				automatic_enable = true,
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			vim.lsp.config("lua_ls", {
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+						workspace = {
+							checkThirdParty = false,
+						},
+					},
+				},
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+}
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -11,7 +11,7 @@ return {
         "json",
         "yaml",
         "go",
-      },
+      }, 
       highlight = {
         enable = true,
       },
